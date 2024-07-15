f3f52035084c4462c2a67935e901bd96accdc05d
Oleksandr Stepanov <oleksandrstepanov.dev@gmail.com>
2024-07-15 19:33:29 +0300

    Enable CaseStudies in the UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,20 @@
-import './App.css';
 import Header from './Header';
 import AboutUs from "./AboutUs.tsx";
 import LogoCarousel from "./LogoCarousel.tsx";
 import Services from "./Services.tsx";
 import Promo from "./Promo.tsx";
+import CaseStudies from "./CaseStudies.tsx";
 import Team from './Team.tsx';
 
 function App() {
   return (
-    <div className="app-container">
+    <div className="max-w-screen-desktop mx-5 desktop:mx-auto">
       <Header />
       <AboutUs />
       <LogoCarousel />
       <Services />
       <Promo />
+      <CaseStudies />
       <Team />
     </div>
   )
